013743c827109c4bd538f0886b342e53b436d7e6
Ondřej Kupka <ondra.cap@gmail.com>
2017-04-03 19:25:05 +0200

    wip [ci skip]

---
+++ charger.go
@@ -0,0 +1,108 @@
+package charger
+
+import (
+	"errors"
+)
+
+var (
+	ErrKeyTaken       = errors.New("key already taken")
+	ErrCyrcleDetected = errors.New("cyrcle detected")
+	ErrNotFound       = errors.New("not found")
+)
+
+type ErrRequired struct {
+	Name string
+}
+
+func (err *ErrRequired) Error() string {
+	return "key required but not set: " + err.Name
+}
+
+type Lookuper interface {
+	Lookup(key string) (string, error)
+}
+
+type Key interface {
+	GetName() string
+	GetDefault() (string, bool)
+	GetRequired() bool
+
+	GetLookupers() []Lookuper
+	GetRenderer() Renderer
+}
+
+type keyRecord struct {
+	Key   Key
+	Value string
+}
+
+type Charger struct {
+	records   map[string]*keyRecord
+	lookupers []Lookuper
+}
+
+func (ch *Charger) AddKey(key Key) error {
+	name := key.GetName()
+
+	if _, ok := ch.records[name]; ok {
+		return ErrKeyTaken
+	}
+
+	ch.records[name] = &keyRecord{key, ""}
+	return nil
+}
+
+func (ch *Charger) MustAddKey(key Key) {
+	if err := ch.AddKey(key); err != nil {
+		panic(err)
+	}
+}
+
+func (ch *Charger) GatherValues() (*Context, error) {
+	ctx := &Context{
+		records: make(map[string]*keyRecord, len(ch.records)),
+	}
+
+KeyLoop:
+	for name, record := range ch.records {
+		for _, lookuper := range record.Key.GetLookupers() {
+			value, err := lookuper.Lookup(name)
+			switch err {
+			case nil:
+				ctx.records[name] = &keyRecord{record.Key, value}
+				continue KeyLoop
+			case ErrNotFound:
+			default:
+				return nil, err
+			}
+		}
+
+		for _, lookuper := range ch.lookupers {
+			value, err := lookuper.Lookup(name)
+			switch err {
+			case nil:
+				ctx.records[name] = &keyRecord{record.Key, value}
+				continue KeyLoop
+			case ErrNotFound:
+			default:
+				return nil, err
+			}
+		}
+
+		value, ok := record.Key.GetDefault()
+		if ok {
+			ctx.records[name] = &keyRecord{record.Key, value}
+			continue KeyLoop
+		}
+
+		if record.Key.GetRequired() {
+			return nil, &ErrRequired{name}
+		}
+	}
+
+	if err := ctx.render(); err != nil {
+		return nil, err
+	}
+
+	return ctx, nil
+}
--- context.go
+++ context.go
@@ -1,124 +1,25 @@
 package charger
 
-import (
-	"errors"
-)
-
-var ErrKeyTaken = errors.New("key already taken")
-
-type ErrRequired struct {
-	Name string
-}
-
-func (err *ErrRequired) Error() string {
-	return "key required but not set: " + err.Name
-}
-
-type Lookuper interface {
-	Lookup(key string) (string, error)
-}
-
-type Key interface {
-	GetName() string
-	GetDefault() (string, bool)
-	GetRequired() bool
-
-	GetLoaders() []Loader
-}
-
-type keyRecord struct {
-	Key   Key
-	Value string
-}
-
-type Charger struct {
-	keys map[string]*keyRecord
-}
-
-func (ch *Charger) AddKey(key Key) error {
-	name := key.GetName()
-
-	if _, ok := ch.records[name]; ok {
-		return ErrKeyTaken
-	}
-
-	ch.records[name] = &keyRecord{key, ""}
-	return nil
-}
-
-func (ch *Charger) MustAddKey(key Key) {
-	if err := ch.AddKey(key); err != nil {
-		panic(err)
-	}
-}
-
-func (ch *Charger) GatherValues() (*Context, error) {
-	ctx := &Context{
-		keys: make(map[string]*keyRecord, len(ch.keys)),
-	}
-
-KeyLoop:
-	for name, record := range ch.keys {
-		var value string
-
-		for _, lookuper := range record.Key.Lookupers() {
-			value, err = lookuper.Lookup(name)
-			switch err {
-			case nil:
-				ctx.keys[name] = &keyRecord{record.Key, value}
-				continue KeyLoop
-			case ErrNotFound:
-			default:
-				return nil, err
-			}
-		}
-
-		for _, lookuper := range ch.lookupers {
-			value, err = lookuper.Lookup(name)
-			switch err {
-			case nil:
-				ctx.keys[name] = &keyRecord{record.Key, value}
-				continue KeyLoop
-			case ErrNotFound:
-			default:
-				return nil, err
-			}
-		}
-
-		value, ok := record.Key.GetDefault()
-		if ok {
-			ctx.keys[name] = &keyRecord{record.Key, value}
-			continue KeyLoop
-		}
-
-		if record.Key.GetRequired() {
-			return nil, &ErrRequired{name}
-		}
-	}
-
-	if err := ctx.render(); err != nil {
-		return nil, err
-	}
-
-	return ctx, nil
+type Context struct {
+	records map[string]*keyRecord
 }
 
 func (ctx *Context) render() error {
-	deps := make(map[string]*[]string, len(ctx.keys))
+	deps := make(map[string][]string, len(ctx.records))
 
-	for name, record := range ctx.keys {
-		depList, err := record.Key.Renderer().Dependencies(record.Value)
+	for name, record := range ctx.records {
+		depList, err := record.Key.GetRenderer().Dependencies(record.Value)
 		if err != nil {
 			return err
 		}
 
-		deps[name] = &depList
+		deps[name] = depList
 	}
 
-	fulfilled := make(map[string]struct{}, len(ctx.keys))
+	fulfilled := make(map[string]struct{}, len(ctx.records))
 
 	for {
-		if len(fulfilled) == len(ctx.keys) {
+		if len(fulfilled) == len(ctx.records) {
 			return nil
 		}
 
